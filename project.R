a6cf8b86855f9fbae672b4160da0e8c60d6ef5b2
Esteban Castillo <esteban.castillojz@udlap.mx>
2017-10-21 18:05:08 -0500

    Project elements
    The final project main elements.

---
+++ project.R
@@ -0,0 +1,138 @@
+# Practical machine learning final project
+# project: One thing that people regularly do is quantify how much of a particular activity they do, 
+# but they rarely quantify how well they do it. In this project, your goal will be to use data from 
+# accelerometers on the belt, forearm, arm, and dumbell of 6 participants.
+
+library(caret)
+
+#Step 1
+
+#Load training and test datasets 
+training <- read.csv("pml-training.csv")
+test <- read.csv("pml-testing.csv")
+
+
+#Step 2
+#Preprocessing steps which includes the elimination of NA values, all the variables that have near zero 
+#variance and all the variables that do not contribute to the classifier performance. This step is applied 
+# to the whole training and test datasets
+
+# Remove variables that are almost always NA
+mostlyNA <- sapply(training, function(x) mean(is.na(x))) > 0.95
+training <- training[, mostlyNA==F]
+test <- test[, mostlyNA==F]
+
+# Remove variables with nearly zero variance
+nzv <- nearZeroVar(training)
+training <- training[, -nzv]
+test <- test[, -nzv]
+
+# Remove variables that do not contribute to the overall classification process
+training <- training[, -(1:5)]
+test <- test[, -(1:5)]
+
+
+#Step 3
+
+#Initialize the classifier based on a Random Forest model using a three cross validation process over train1 subset
+
+Parameters <- trainControl(method="cv", number=3, verboseIter=F)
+fit <- train(classe ~ ., data=train1, method="rf", trControl=Parameters )
+
+
+#Print the obtained model to see the overall classifier accuracy
+print(fit$finalModel)
+
+# Call:
+#   randomForest(x = x, y = y, mtry = param$mtry) 
+# Type of random forest: classification
+# Number of trees: 500
+# No. of variables tried at each split: 27
+# 
+# OOB estimate of  error rate: 0.18%
+# Confusion matrix:
+#   A    B    C    D    E  class.error
+# A 4462    2    0    0    0 0.0004480287
+# B    5 3030    2    1    0 0.0026333114
+# C    0    5 2733    0    0 0.0018261505
+# D    0    0    9 2563    1 0.0038865138
+# E    0    1    0    2 2883 0.0010395010
+
+#From the model creation it can be observed thta the algorithm decid to create 500 trees, each one with 27 variables associated.
+
+#Step 4
+
+# Using the previously created model, the unpredicted instances are classified in order to obtain the labels for each of the 
+# elements in the test dataset
+
+
+# Predict elements on the  test dataset
+predictions <- predict(fit, newdata=test)
+
+#To evaluate accuracy
+pedictionsEvaluation<-predictions
+
+# Convert predictions to character vector
+predictions <- as.character(predictions)
+
+
+#Step 5
+
+#Save the predictons to a file
+
+# Number of predictions made
+n <- length(predictions)
+
+#Save all predictions in a single file
+pred <- c()
+for(i in 1:n) {
+  pred <- c(pred, paste0("problem_id_", i," ", predictions[i],"\n"))
+}
+write.table(pred, file="predictions.txt", quote=F, row.names=F, col.names=F)
+
+
+#Step 6
+#After  final quiz extract goldStandar to evaluate the accuracy 
+goldStandar<-c("B","A","B","A","A","E","D","B","A","A","B","C","B","A","E","E","A","B","B","B")
+
+# show confusion matrix to get estimate of out-of-sample error
+print(confusionMatrix(goldStandar, pedictionsEvaluation))
+
+# Confusion Matrix and Statistics
+# 
+# Reference
+# Prediction A B C D E
+# A 7 0 0 0 0
+# B 0 8 0 0 0
+# C 0 0 1 0 0
+# D 0 0 0 1 0
+# E 0 0 0 0 3
+# 
+# Overall Statistics
+# 
+# Accuracy : 1          
+# 95% CI : (0.8316, 1)
+# No Information Rate : 0.4        
+# P-Value [Acc > NIR] : 1.1e-08    
+# 
+# Kappa : 1          
+# Mcnemar's Test P-Value : NA         
+# 
+# Statistics by Class:
+# 
+# Class: A Class: B Class: C Class: D Class: E
+# Sensitivity              1.00      1.0     1.00     1.00     1.00
+# Specificity              1.00      1.0     1.00     1.00     1.00
+# Pos Pred Value           1.00      1.0     1.00     1.00     1.00
+# Neg Pred Value           1.00      1.0     1.00     1.00     1.00
+# Prevalence               0.35      0.4     0.05     0.05     0.15
+# Detection Rate           0.35      0.4     0.05     0.05     0.15
+# Detection Prevalence     0.35      0.4     0.05     0.05     0.15
+# Balanced Accuracy        1.00      1.0     1.00     1.00     1.00
+
+
+# According to the confusion matrix, the accuracy of the model is one which highlights that the features selected as well
+# as the classifier are the correct ones to predict this kind of information, however more experiments need to be performed 
+# in order  to test the model using more test samples from distinct  types of accelerometers.
+
+
